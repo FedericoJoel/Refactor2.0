44c4c719c064f7290a47be7e78ebf91be2b86c8d
LucasBlanco <lucas_manuel_blanco@hotmail.com>
2018-09-18 14:14:06 -0300

    arreglo paginador de solicitudes

--- js/controllers.js
+++ js/controllers.js
@@ -440,7 +440,44 @@ angular.module('GestionarApp.controllers', ['angular-loading-bar', 'GestionarApp
 
 
   .controller('solicitudesCrt', function ($scope, $http, $mdDialog, UserSrv, $filter, Permisos) {
+    /*Esto esta copiado de la directiva del paginador */
+      $scope.primeraPagina = 1 // La primera pagina de laspaginas mostradas en el footer para paginar
+      $scope.ActualPage =1 // pagina en la que estoy posicionado
+      var elemsPorPagina = 10
+      var aumentoPaginas = 5
+      $scope.changeElemsPorPagina = function(cant){
+          elemsPorPagina = cantidad
+      }
+      $scope.getPaginas = function (elems) { // Devuelve un array con un numero ascendente por cada elemento de la lista, necesario cuando la lista se modifica por algun filtro
+          if (elems) {
+              var cantPaginas = (new Array(Math.ceil(elems.length / elemsPorPagina)).fill(0).map((x, index) => index + 1))
+              return cantPaginas
+          }
+      }
+      $scope.aumentarPagina = function (algo) { // Mueve el numero de pag mostradas en el footer para arriba una cant igual a aumentoPaginas
+          if (($scope.getPaginas(algo).length - $scope.primeraPagina) > elemsPorPagina) {
+              $scope.primeraPagina = $scope.primeraPagina + aumentoPaginas
+          }
+      }
+      $scope.disminuirPagina = function () { // Mueve el numero de pag mostradas en el footer para abajo una cant igual a aumentoPaginas
+          if($scope.primeraPagina > 1)
+              $scope.primeraPagina = $scope.primeraPagina - aumentoPaginas
+      }
+      $scope.ChangePage = function (pag) {
+          $scope.ActualPage = pag;
+      }
+      $scope.separarPaginas= function(index){ // Pagina la tabla, dejando visibles una cant de paginas igual a elemsPorPagina
+          var x1 = (index <= ($scope.ActualPage * elemsPorPagina))
 
+          var x2 = (index >= ($scope.ActualPage - 1) * elemsPorPagina)
+
+          return x1 && x2
+      }
+      $scope.getPrimeraPagina= function(){
+          $scope.ActualPage =1
+          $scope.primeraPagina =1
+      }
+      /*Esto esta copiado de la directiva del paginador */
     var socket = io.connect("http://node-gestionar.herokuapp.com:80");
     socket.emit('storeClientInfo', {
       customId: localStorage.getItem('user_id')
