b8950c78f6746ac09514260babd3c32a26a9f783
LucasBlanco <lucas_manuel_blanco@hotmail.com>
2018-09-21 11:34:04 -0300

    Arreglo el telefono en la edicion de los medicos

--- js/controllers.js
+++ js/controllers.js
@@ -443,19 +443,19 @@ angular.module('GestionarApp.controllers', ['angular-loading-bar', 'GestionarApp
     /*Esto esta copiado de la directiva del paginador */
       $scope.primeraPagina = 1 // La primera pagina de laspaginas mostradas en el footer para paginar
       $scope.ActualPage =1 // pagina en la que estoy posicionado
-      var elemsPorPagina = 10
+      $scope.elemsPorPagina = 10
       var aumentoPaginas = 5
       $scope.changeElemsPorPagina = function(cant){
           elemsPorPagina = cantidad
       }
       $scope.getPaginas = function (elems) { // Devuelve un array con un numero ascendente por cada elemento de la lista, necesario cuando la lista se modifica por algun filtro
           if (elems) {
-              var cantPaginas = (new Array(Math.ceil(elems.length / elemsPorPagina)).fill(0).map((x, index) => index + 1))
+              var cantPaginas = (new Array(Math.ceil(elems.length / $scope.elemsPorPagina)).fill(0).map((x, index) => index + 1))
               return cantPaginas
           }
       }
       $scope.aumentarPagina = function (algo) { // Mueve el numero de pag mostradas en el footer para arriba una cant igual a aumentoPaginas
-          if (($scope.getPaginas(algo).length - $scope.primeraPagina) > elemsPorPagina) {
+          if (($scope.getPaginas(algo).length - $scope.primeraPagina) >  $scope.elemsPorPagina) {
               $scope.primeraPagina = $scope.primeraPagina + aumentoPaginas
           }
       }
@@ -467,9 +467,9 @@ angular.module('GestionarApp.controllers', ['angular-loading-bar', 'GestionarApp
           $scope.ActualPage = pag;
       }
       $scope.separarPaginas= function(index){ // Pagina la tabla, dejando visibles una cant de paginas igual a elemsPorPagina
-          var x1 = (index <= ($scope.ActualPage * elemsPorPagina))
+          var x1 = (index <= ($scope.ActualPage *  $scope.elemsPorPagina))
 
-          var x2 = (index >= ($scope.ActualPage - 1) * elemsPorPagina)
+          var x2 = (index >= ($scope.ActualPage - 1) *  $scope.elemsPorPagina)
 
           return x1 && x2
       }
