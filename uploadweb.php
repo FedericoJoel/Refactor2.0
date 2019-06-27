ffffe95e24ab6973cfb7face8a0524e03ff5d5f4
jaccinelli <juanpabloaccinelli@gmail.com>
2019-06-26 22:47:31 -0300

    uploadweb

--- uploadweb.php
+++ uploadweb.php
@@ -1,14 +1,46 @@
-<?php
-
- 
-$location = $_POST['directory'];
-//$uploadfile = $_POST['fileName'];
-$uploadfilename = $_FILES['file']['tmp_name'];
-$uploadfile = $_FILES['file']['name'];
- 
-if(move_uploaded_file($uploadfilename, 'certificados/'.$uploadfile)){
-        echo 'File successfully uploaded!';
-} else {
-        echo 'Upload error!';
+<?php 
+header('Content-Type: application/json; charset=utf-8');
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+$response = array();
+$upload_dir = 'certifcados/';
+if($_FILES['file'])
+{
+    $file_name = $_POST['nombrefile'];
+    $file_tmp_name = $_FILES["file"]["tmp_name"];
+    $error = $_FILES["file"]["error"];
+    if($error > 0){
+        $response = array(
+            "status" => "error",
+            "error" => true,
+            "message" => "Error uploading the file!"
+        );
+    }else 
+    {
+    
+        if(move_uploaded_file($file_tmp_name ,  '/home/gestiona/public_html/certificados/'.$file_name)) {
+            $response = array(
+                "status" => "success",
+                "error" => false,
+                "message" => "File uploaded successfully",
+                "url" => "/".$file_name
+              );
+        }else
+        {
+            $response = array(
+                "status" => "error",
+                "error" => true,
+                "message" => "Error uploading the file!"
+            );
+        }
+    }
+    
+}else{
+    $response = array(
+        "status" => "error",
+        "error" => true,
+        "message" => "No file was sent!"
+    );
 }
+echo json_encode($response);
 ?>
